c071de91d93fb3d30ff311b3e20951beae003fba
José Miguel Franco <josemi_fv@hotmail.com>
2014-06-24 23:53:26 +0200

    Corregido encoding

--- practica1/mapper.py
+++ practica1/mapper.py
@@ -2,15 +2,15 @@
 
 import sys
 
-# Lectura de datos de la entrada estándar
+# Lectura de datos de la entrada estandar
 for line in sys.stdin:
     # Eliminamos espacios en blanco al comienzo y al final
     line = line.strip()
-    # Partimos la línea en palabras
+    # Partimos la linea en palabras
     words = line.split()
     # Incrementamos los contadores
     for word in words:
-        # Escritura de los datos por la salida estándar
-        # La salida del mapper será la entrada del reducer
-        # En esta versión se separan clave y valor por un tabulador
+        # Escritura de los datos por la salida estandar
+        # La salida del mapper sera la entrada del reducer
+        # En esta version se separan clave y valor por un tabulador
         print '%s\t%s' % (word, 1)
--- practica1/reducer.py
+++ practica1/reducer.py
@@ -7,7 +7,7 @@ current_word = None
 current_count = 0
 word = None
 
-# Lectura de datos de la entrada estándar
+# Lectura de datos de la entrada estandar
 for line in sys.stdin:
     # Eliminamos espacios en blanco al comienzo y al final
     line = line.strip()
@@ -15,12 +15,12 @@ for line in sys.stdin:
     # Parseamos la salida del mapper.py
     word, count = line.split('\t', 1)
 
-    # Convertimos el número de ocurrencias a entero
+    # Convertimos el numero de ocurrencias a entero
     try:
         count = int(count)
     except ValueError:
         # Error al convertir a entero
-        # Se descarta la línea silenciosamente
+        # Se descarta la linea silenciosamente
         continue
 
     # Este IF funciona porque Hadoop ejecuta la etapa de SORT
@@ -28,13 +28,13 @@ for line in sys.stdin:
     if current_word == word:
         current_count += count
     else:
-        # Hack para controlar el caso de la primera línea
+        # Hack para controlar el caso de la primera linea
         if current_word:
             # Escritura de los datos por la salida estándar
             print '%s\t%s' % (current_word, current_count)
         current_count = count
         current_word = word
 
-# ¡No hay que olvidar la última entrada!
+# ¡No hay que olvidar la ultima entrada!
 if current_word == word:
     print '%s\t%s' % (current_word, current_count)
